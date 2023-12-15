323cfcb7552846d29ba0adc94e0eae54cb5c6b68
Marco Upia <markremembers@gmail.com>
2023-12-15 13:57:10 -0500

    solved

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        new_anagram = []
+        for word in word_list:
+            if sorted(word) == sorted(self.word):
+                new_anagram.append(word)
+        return new_anagram
+
+
+listen = Anagram("listen")
+result = listen.match(['enlists', 'google', 'inlets', 'banana', 'silent'])
+print(result)
